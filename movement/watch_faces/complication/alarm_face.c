214d1a777fb2625a919c99b4ba6715dc320d9f37
tomekskuta <kontakt@tomekskuta.pl>
2023-01-21 13:53:40 +0100

    [fix] Remove date visibility in simple watch

--- movement/watch_faces/complication/alarm_face.c
+++ movement/watch_faces/complication/alarm_face.c
@@ -408,8 +408,8 @@ bool alarm_face_loop(movement_event_t event, movement_settings_t *settings, void
         movement_move_to_next_face();
         break;
     case EVENT_MODE_LONG_PRESS:
-            movement_move_to_face(0);
-            break;
+        movement_move_to_face(0);
+        break;
     case EVENT_TIMEOUT:
         movement_move_to_face(0);
         break;
